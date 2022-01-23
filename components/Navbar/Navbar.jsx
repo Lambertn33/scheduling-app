fd01fe6e38d9bf47964cc1821b5a714ec163c343
Lambert <lambertgithub@gmail.com>
2022-01-23 20:44:53 +0200

    Adding Provider

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { BiLink } from 'react-icons/bi'
 import { BsCalendarFill } from 'react-icons/bs'
 import { HiLogout } from 'react-icons/hi'
 
-export default function Navbar({ ses }) {
+export default function Navbar({ session }) {
     return (
         <div className='h-screen border-r-2'>
             <div className='flex flex-col justify-between h-full p-4 border'>
@@ -32,7 +32,7 @@ export default function Navbar({ ses }) {
                  <Image src="/images/avatar.png" className="p-8 bg-gray-500 rounded-full" width={50} height={50}/>
                  </div>
                   <div className='flex flex-col items-center'>
-                      <span className='font-bold'>{ses.user.username}</span>
+                      <span className='font-bold'>{session.user.username}</span>
                       <div className='flex items-center'>
                           <HiLogout />
                          <span onClick={signOut} className='ml-3 cursor-pointer font-semi-bold'>Logout</span>
